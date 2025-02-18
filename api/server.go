d48e6a8f4cb0c6be55264728d5342d8bd28ab97c
Mitchxxx <megboko@gmail.com>
2025-02-18 12:34:44 +0000

    Added JWT and Paseto

--- api/server.go
+++ api/server.go
@@ -1,28 +1,47 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/mitchxxx/simplebank/db/sqlc"
+	"github.com/mitchxxx/simplebank/token"
+	"github.com/mitchxxx/simplebank/util"
 )
 
 //Server serves HTTP request for our Banking service
 
 type Server struct {
+	config util.Config
 	store db.Store
 	router *gin.Engine
+	tokenMaker token.Maker
 }
 
 // NewServer creates a new HTTP server and setup routing
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
-
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	//tokenMaker, err := token.NewJWTMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot create a token maker : %w", err)
+	}
+	server := &Server{
+		config: config,
+		store: store,
+		tokenMaker: tokenMaker,
+	}
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -31,9 +50,9 @@ func NewServer(store db.Store) *Server {
 	router.POST("/transfers", server.CreateTransfer)
 
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	server.router = router
-	return server
 }
 
 // Start runs the HTTP server on  a specific address
